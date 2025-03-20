8047d6fa556c71c29f235ca0cea629053b5e649e
nstprod <milana.prod@gmail.com>
2025-03-20 13:19:50 +0100

    style changes, more tests

--- test2.c
+++ test2.c
@@ -40,7 +40,7 @@ void save_test(FILE * fp){
     fprintf(fp, "test\n");
 }
 
-int analyze_rgraph(int opt, int e, int n, FILE *out, int type){
+int analyze_rgraph( int e, int n, FILE *out, int type){
     char ch;
     int a_e = 0; int a_n = 0; int a_t = 0; // actual edges, nodes, type
     int conditions[3] = {0};
@@ -51,7 +51,7 @@ int analyze_rgraph(int opt, int e, int n, FILE *out, int type){
     char line[50];
 
     if (fgets(line, sizeof(line), out)) {
-        printf("%s\n", line);
+        printf("%s", line);
         if (type == 1) {
             if (strstr(line, "digraph")) conditions[0] = 1;
             else if (strstr(line, "graph")) a_t = 2;
@@ -64,10 +64,12 @@ int analyze_rgraph(int opt, int e, int n, FILE *out, int type){
 
     if (conditions[0] == 1) a_t = type;
     
+
     
     while (fgets(line, sizeof(line), out)){
+        printf("%s", line);
         if (sscanf(line, " %d%*s%d", &n1, &n2) == 2){
-            //printf("%s \n n1 = %d n2 = %d \n", line, n1, n2);
+            
             if (n1 >= 0 && n1 < n)
                 nodes_values[n1]++;
             if (n2 >= 0 && n2 < n)
@@ -75,25 +77,28 @@ int analyze_rgraph(int opt, int e, int n, FILE *out, int type){
             a_e++;
         }
     }
-
-    printf("Test %d details: \n", opt);
-
-    printf("Nodes encountered:\n");
+    int *missing_nodes = malloc(sizeof(int)*n);
+    printf("    Nodes encountered: ");
+    int m = 0;
     for (int i = 0; i < n; i++){
         if (nodes_values[i] > 0) {
-            printf("%d\n", i);
+            printf("%d ", i);
             a_n++;
         }
-        else printf("missing node %d\n", i);
+        else missing_nodes[m++] = i ;
     }
-
+    printf("\n");
+    if (m>0)
+        for (int i = 0; i < m; i++)
+                printf("     missing node %d\n", missing_nodes[i]);
+            
     if (a_n == n) conditions[1] = 1;
     if (a_e == e) conditions[2] = 1;
 
     
-    printf("condition 1 (graph type)  = %d: type %d  (expected %d)\n", conditions[0], a_t, type);
-    printf("condition 2 (node number) = %d: %d nodes (expected %d)\n", conditions[1], a_n, n);
-    printf("condition 3 (edge number) = %d: %d edges (expected %d)\n\n", conditions[2], a_e, e);
+    printf("    condition 1 (graph type)  is %s: %d (expected %d)\n", conditions[0] == 1 ? "  met" : "unmet", a_t, type);
+    printf("    condition 2 (node number) is %s: %d (expected %d)\n", conditions[1] == 1 ? "  met" : "unmet", a_n, n);
+    printf("    condition 3 (edge number) is %s: %d (expected %d)\n\n", conditions[2] == 1 ? "  met" : "unmet", a_e, e);
 
     if (conditions[0] == 1 &&
         conditions[1] == 1 &&
@@ -104,27 +109,32 @@ int analyze_rgraph(int opt, int e, int n, FILE *out, int type){
 }
 
 int test(FILE * fp, int opt, int argc, char **argv){
-    char line[50];
+    char line[100];
     int g_type = 1;
     int e, n, d;
     char req[100];
+
+    printf("> details:\n");
     switch (opt) {
         case 1:
-            manual_mode(3, 3, fp, g_type, 2);
+            e = 3; n = 3; d = 2;
+            manual_mode(e, n, fp, g_type, d);
             // 3 mnode and 3 edges graph 
             fprintf(fp, "0 1\n");
             fprintf(fp, "0 2\n");
             fprintf(fp, "2 1\n");
             break;
         case 2:
-            manual_mode(3, 3, fp, g_type, 1);
+            e = 3; n = 3; d = 1;
+            manual_mode(e, n, fp, g_type, d);
             // 3 mnode and 3 edges graph 
             fprintf(fp, "0 1\n");
             fprintf(fp, "0 2\n");
             fprintf(fp, "2 1\n");
             break;
         case 3:
-            manual_mode(3, 5, fp, g_type, 1);
+            e = 3; n = 5;d = 1;
+            manual_mode(e, n, fp, g_type, d);
             // 3 mnode and 3 edges graph 
             fprintf(fp, "0 1\n");
             fprintf(fp, "0 1\n"); //this edge exists
@@ -134,32 +144,54 @@ int test(FILE * fp, int opt, int argc, char **argv){
             break;
 
         case 4:
-            manual_mode(5, 5, fp, g_type, 2);
+            e = 5; n = 5;d = 2;
+            manual_mode(e, n, fp, g_type, d);
             fprintf(fp, "0 1\n");
             fprintf(fp, "0 2\n"); 
             fprintf(fp, "2 3\n"); 
             fprintf(fp, "0 4\n");
             fprintf(fp, "2 4\n");
             break;
-        case 5:
+        case 5: // random graphs
             g_type = 2;
-            e = 10; n = 8; d = 1;
+            e = 5; n = 5; d = 1;
+            break;
+
+        case 6:
+            g_type = 2;
+            e = 20; n = 20; d = 2;
+            break;
+        
+        case 7:
+            g_type = 2;
+            e = 10; n = 9; d = 1;
             break;
             
-        case 6: 
+        case 8: 
             g_type = 3;
-            e = 13; n = 11; d = 1;
-            strcpy(req, "Create a directed graph with 11 vertices and 13 edges\n");
+            e = 13; n = 11; d = 2;
+            strcpy(req, "Create an udirected graph with 11 vertices and 13 edges\n");
             break;
 
     }
+
+    printf("    Enabled '%s'\n", g_type != 3 ? "manual parameters input" : "chat mode");
+    if (g_type != 3){
+        printf("    Enabled '%s'\n", g_type == 1 ? "manual edge input" : "Random graph");
+        printf("    Enabled '%s' graph type\n", d == 1 ? "Directed" : "Undirected");
+    }
+    printf("    Asked for %d nodes\n", n);
+    printf("    Asked for %d edges\n", e);
 
     if (g_type == 2){
         manual_mode(e, n, fp, g_type, d);
     }
     else if (g_type == 3){
+       
         llm_mode(fp);
+        
         fprintf(fp, "%s", req);
+        printf("    Waiting for AI response...\n");
         while (strstr(line, "Raw response:")){
             fgets(line, sizeof(line), fp);
         }
@@ -183,7 +215,7 @@ int test(FILE * fp, int opt, int argc, char **argv){
     }
     
     else if (g_type == 2 || g_type == 3){
-        int r = analyze_rgraph(opt, e, n, out, d);
+        int r = analyze_rgraph( e, n, out, d);
             return r;
 
     }
@@ -193,9 +225,9 @@ int test(FILE * fp, int opt, int argc, char **argv){
 }
 
 
-void print_test_status(int *status){
-
-    for(int i =0; i<5; i++){
+void print_test_status(int *status, int n){
+    printf("RESULTS:\n");
+    for(int i =0; i<n; i++){
         if(status[i]==1){
             printf("Test[%d] passed\n",i+1);
         }else{
@@ -210,24 +242,29 @@ int main(int argc, char **argv){
     int status;
     char path[PATH_MAX];
 
-    int test_status[5];
+    int t_num = 8;
+    int *test_status = malloc(sizeof(int) * t_num);
 
-    for (int i = 1; i <= 6 ; i++){
+    for (int i = 1; i <= t_num ; i++){
 
-        FILE *fp = popen("stdbuf -oL ./graph_cli", "w");
+        FILE *fp = popen("./graph_cli > /dev/null 2>&1", "w"); // remove stdbuf -oL
     
-        if (fp == NULL)
-            /* Handle error */;
-           
+        
+            
+        /*
         fgets(path, PATH_MAX, fp);
             printf("%s", path);
-
-        int r = test(fp, i, argc, argv);
-    
-        test_status[i-1] = r;
+        */
+       if (fp != NULL){
+            printf("Test %d STARTED\n", i);
+            int r = test(fp, i, argc, argv);
+            printf("Test %d FINISHED: test resulted in %s\n\n", i, r == 1 ? "SUCCESS" : "FAILURE");
+            test_status[i-1] = r;
+       }
+        
     
     }
-    print_test_status(test_status);
+    print_test_status(test_status, t_num);
     
     return 0;
 }
